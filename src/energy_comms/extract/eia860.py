ea1aaf92268ba5c599a42b7324f812dcf1ddc5c4
Katie Lamb <katherine.lamb@catalyst.coop>
2022-10-29 14:43:19 -0700

    extract eia860

---
+++ src/energy_comms/extract/eia860.py
@@ -0,0 +1,17 @@
+"""Extract EIA 860 plants data and load into pandas dataframe."""
+import pandas as pd
+import sqlalchemy as sa
+
+import pudl
+
+
+def extract() -> pd.DataFrame:
+    """Extract EIA 860 generators PUDL output table.
+
+    PUDL workspace settings need to be set to point to PUDL DB.
+    See PUDL documentation for more info.
+    """
+    pudl_engine = sa.create_engine(pudl.workspace.setup.get_defaults()["pudl_db"])
+    pudl_out = pudl.output.pudltabl.PudlTabl(pudl_engine, freq="AS")
+    gens = pudl_out.gens_eia860()
+    return gens

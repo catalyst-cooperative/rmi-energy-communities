05d5f49c8bfed2e25292dcea8786f75f43ff98db
Katie Lamb <katherine.lamb@catalyst.coop>
2022-12-17 21:40:45 -0800

    switched to lat lon to census geom

--- src/energy_comms/transform/epa.py
+++ src/energy_comms/transform/epa.py
@@ -1,12 +1,20 @@
 """Transform functiosn for EPA brownfields data."""
 import logging
+from typing import Any, Literal
 
+import geopandas
 import pandas as pd
+
+import energy_comms
 
 logger = logging.getLogger(__name__)
 
 
-def transform(raw_df: pd.DataFrame) -> pd.DataFrame:
+def transform(
+    raw_df: pd.DataFrame,
+    census_geometry: Literal["state", "county", "tract"] = "county",
+    pudl_settings: dict[Any, Any] | None = None,
+) -> pd.DataFrame:
     """Map zip codes to counties, prepare dataframe for map integration."""
     logger.info("Transforming EPA brownfields data.")
     # from HUD: https://www.huduser.gov/portal/datasets/usps_crosswalk.html
@@ -14,16 +22,21 @@ def transform(raw_df: pd.DataFrame) -> pd.DataFrame:
     df = raw_df.copy()
     # make lower case and replace spaces
     df.columns = df.columns.str.lower().str.replace(" ", "_")
-
     # assign dtypes
     df = df.astype(
         {"site_name": str, "state": str, "latitude": float, "longitude": float}
     )
-    df["site_name"] = df["site_name"].str.title()
+    # needed for eventual merge with other criteria
+    df["area_title"] = df["site_name"].str.strip().str.title()
+    df = df.dropna(subset=["latitude", "longitude"])
+    df = geopandas.GeoDataFrame(
+        df, geometry=geopandas.points_from_xy(df.longitude, df.latitude)
+    )
+    df = energy_comms.helpers.get_geometry_intersection(
+        df, census_geometry=census_geometry, pudl_settings=pudl_settings
+    )
 
-    # only keep columns we want
-
-    df = df[["site_name", "zip_code", "latitude", "longitude", "state"]]
+    """
     # read in zip code cross walk from HUD
 
     zip_code_crosswalk = pd.read_excel(
@@ -34,18 +47,11 @@ def transform(raw_df: pd.DataFrame) -> pd.DataFrame:
     zip_crosswalk = dict(zip(zip_code_crosswalk["zip"], zip_code_crosswalk["county"]))
 
     # fill in criteria for patio and map crosswalk
+    """
 
     df = df.assign(
-        fips_county=df.zip_code.map(zip_crosswalk),
         qualifying_area="site",
         criteria="brownfield",
     )
-
-    # not needed for final output
-
-    df = df.drop(columns=["zip_code", "state"])
-
-    # needed for eventual merge with other criteria
-    df = df.rename(columns={"site_name": "area_title"})
 
     return df
--- tests/integration/etl_test.py
+++ tests/integration/etl_test.py
@@ -74,14 +74,18 @@ def test_eia860_etl(
         raise AssertionError("adjacent_id_fips not in transformed EIA 860 dataframe.")
 
 
-def test_epa_etl() -> None:
+def test_epa_etl(pudl_settings_fixture: dict[Any, Any] | None) -> None:
     """Verify that we can ETL the EPA brownfields data."""
     raw_df = energy_comms.extract.epa.extract()
     if raw_df.empty:
         raise AssertionError("EPA extract returned empty dataframe.")
     logger.info("Running EPA transform.")
-    df = energy_comms.transform.epa.transform(raw_df)
+    df = energy_comms.transform.epa.transform(
+        raw_df, pudl_settings=pudl_settings_fixture
+    )
     if df.empty:
         raise AssertionError("EPA transform returned empty dataframe.")
-    if not df[(df.fips_county.str.len() != 5) & ~(df.fips_county.isnull())].empty:
-        raise AssertionError("EPA county FIPS column is not all 5 character strings.")
+    if not df[(df.county_id_fips.str.len() != 5) & ~(df.county_id_fips.isnull())].empty:
+        raise AssertionError(
+            "EPA county ID FIPS column is not all 5 character strings."
+        )

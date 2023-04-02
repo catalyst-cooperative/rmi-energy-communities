cffbe09aa1897f2ff40ba02c8444c02756722d5e
Katie Lamb <katherine.lamb@catalyst.coop>
2023-04-01 23:12:48 -0700

    bump to python 3.11

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     license="MIT",
     # Fill in search keywords that users might use to find the package
     keywords=[],
-    python_requires=">=3.10,<3.12",
+    python_requires=">=3.11,<3.12",
     # In order for the dependabot to update versions, they must be listed here.
     # Use the format pkg_name>=x,<y", Included packages are just examples:
     install_requires=[
